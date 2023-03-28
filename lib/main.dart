8516d32107e9d425e0d3f9669a60de0ec00fcbdb
jylee-lamplight <j.lee@lamplight.co.jp>
2023-03-28 17:56:20 +0900

    c1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:math';
 
 import 'package:background_location_tracker/background_location_tracker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:intl/intl.dart';
 import 'package:logger/logger.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:test_alway_location/pages/p1_tap_main_page.dart';
@@ -11,7 +15,7 @@ import 'define/define.dart';
 import 'models/localLocationData.dart';
 
 @pragma('vm:entry-point')
-void backgroundCallback() {
+Future<void> backgroundCallback() async {
   BackgroundLocationTrackerManager.handleBackgroundUpdated(
     (data) async => Repo().update(data),
   );
@@ -19,7 +23,6 @@ void backgroundCallback() {
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Utils.init();
   await BackgroundLocationTrackerManager.initialize(
     backgroundCallback,
     config: BackgroundLocationTrackerConfig(
@@ -55,19 +58,81 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Repo  {
-  var logger = Logger();
+class Repo {
   static Repo? _instance;
 
   Repo._();
 
   factory Repo() => _instance ??= Repo._();
+
+  Future<void> update(BackgroundLocationUpdateData data) async {
+    final text = 'Location Update: Lat: ${data.lat} Lon: ${data.lon}';
+    print(text); // ignore: avoid_print
+    // sendNotification(text);
+    await LocationDao().saveLocation(LocalLocationData(DateTime.now(), data.lat, data.lon, true));
+  }
+}
+
+class LocationDao {
+  static const _locationsKey = 'LocalLocationData';
+  static const _locationSeparator = '-/-/-/';
+
+  static LocationDao? _instance;
+
+  LocationDao._();
+
+  factory LocationDao() => _instance ??= LocationDao._();
+
   SharedPreferences? _prefs;
+
   Future<SharedPreferences> get prefs async =>
       _prefs ??= await SharedPreferences.getInstance();
 
-  Future<void> update(BackgroundLocationUpdateData data) async {
-    await Utils.setLocationHistory(
-        LocalLocationData(DateTime.now(), data.lat, data.lon, true));
+  Future<void> saveLocation(LocalLocationData data) async {
+    List<LocalLocationData> locations = await getLocations();
+    locations.add(data);
+    await (await prefs)
+        .setString(_locationsKey, jsonEncode({"list": locations}));
   }
+
+  Future<List<LocalLocationData>> getLocations() async {
+    final prefs = await this.prefs;
+    await prefs.reload();
+    final locationsString = prefs.getString(_locationsKey);
+    if (locationsString == null) return [];
+    List<dynamic> list = jsonDecode(locationsString)["list"];
+    List<LocalLocationData> result = [];
+    for (var i = 0; i < list.length; i++) {
+      result.add(LocalLocationData.fromJson(list[i]));
+    }
+    return result;
+  }
+
+  Future<void> clear() async => (await prefs).clear();
+}
+
+void sendNotification(String text) {
+  const settings = InitializationSettings(
+    android: AndroidInitializationSettings('app_icon'),
+    iOS: DarwinInitializationSettings(
+      requestAlertPermission: false,
+      requestBadgePermission: false,
+      requestSoundPermission: false,
+    ),
+  );
+  FlutterLocalNotificationsPlugin().initialize(
+    settings,
+    onDidReceiveNotificationResponse: (data) async {
+      print('ON CLICK $data'); // ignore: avoid_print
+    },
+  );
+  FlutterLocalNotificationsPlugin().show(
+    Random().nextInt(9999),
+    'Title',
+    text,
+    const NotificationDetails(
+      android: AndroidNotificationDetails('test_notification', 'Test'),
+      iOS: DarwinNotificationDetails(),
+    ),
+  );
 }
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -8,25 +8,18 @@ import '../define/define.dart';
 import '../models/localLocationData.dart';
 
 class Utils {
-  static late final SharedPreferences _prefs;
-
-  static bool _init = false;
-  static Future init() async {
-    if (_init) return;
-    _prefs = await SharedPreferences.getInstance();
-    _init = true;
-    return _prefs;
-  }
 
   static Future<void> setLocationHistory(LocalLocationData newData) async {
     var logger = Logger();
-    List<LocalLocationData> old = await getLocationHistory();
+    SharedPreferences _prefs = await SharedPreferences.getInstance();
+
+    logger.d("setLocationHistory:");
+    List<LocalLocationData> old = await Utils.getLocationHistory();
     newData.isAppActive = await Utils.getActive();
     old.add(newData);
 
     String jsonNew = jsonEncode({"list": old});
-    await _prefs.setString(
-         Define.KEYSTORE_LOCAL_LOCATION_DATA, jsonNew);
+    await _prefs.setString(Define.KEYSTORE_LOCAL_LOCATION_DATA, jsonNew);
     logger.d("setLocationHistory:");
   }
 
@@ -35,15 +28,17 @@ class Utils {
   /// @param LocalLocationData 位置情報
   static Future<List<LocalLocationData>> getLocationHistory() async {
     var logger = Logger();
-    String? value =
-    _prefs.getString( Define.KEYSTORE_LOCAL_LOCATION_DATA);
+    SharedPreferences _prefs = await SharedPreferences.getInstance();
+
+    await _prefs.reload();
+    String? value = _prefs.getString(Define.KEYSTORE_LOCAL_LOCATION_DATA);
 
     if (value == null) {
       return [];
     }
 
     List<dynamic> list = jsonDecode(value)["list"];
-    logger.d("getLocationHistory:"+list.length.toString());
+    logger.d("getLocationHistory:" + list.length.toString());
 
     String lastTime = "";
 
@@ -65,15 +60,17 @@ class Utils {
 
   static Future<List<LocalLocationData>> getLocationHistoryReverse() async {
     var logger = Logger();
-    String? value =
-    _prefs.getString( Define.KEYSTORE_LOCAL_LOCATION_DATA);
+    SharedPreferences _prefs = await SharedPreferences.getInstance();
+
+    await _prefs.reload();
+    String? value = _prefs.getString(Define.KEYSTORE_LOCAL_LOCATION_DATA);
 
     if (value == null) {
       return [];
     }
 
     List<dynamic> list = jsonDecode(value)["list"];
-    logger.d("getLocationHistory:"+list.length.toString());
+    logger.d("getLocationHistory:" + list.length.toString());
 
     String lastTime = "";
 
@@ -82,7 +79,7 @@ class Utils {
       LocalLocationData target = LocalLocationData.fromJson(list[i]);
 
       String targetTime =
-      DateFormat('yyyy-MM-dd hh:mm').format(target.locationDateTime);
+          DateFormat('yyyy-MM-dd hh:mm').format(target.locationDateTime);
       if (lastTime.isEmpty || targetTime != lastTime) {
         datas.add(target);
         lastTime =
@@ -94,23 +91,32 @@ class Utils {
   }
 
   static Future<void> clearHistory() async {
+    SharedPreferences _prefs = await SharedPreferences.getInstance();
+
     _prefs.clear();
   }
 
-  static Future<void> setActive() async {
-    var logger = Logger();
+  static Future<void> setActive() async {    var logger = Logger();
+
+  SharedPreferences _prefs = await SharedPreferences.getInstance();
+
     logger.d("Active");
-    await _prefs.setBool( "active", true);
+    await _prefs.setBool("active", true);
   }
 
   static Future<void> inActive() async {
     var logger = Logger();
+    SharedPreferences _prefs = await SharedPreferences.getInstance();
+
     logger.d("InActive");
-    await _prefs.setBool( "active", false);
+    await _prefs.setBool("active", false);
   }
 
   static Future<bool> getActive() async {
-    bool? result = _prefs.getBool( "active");
-    return result??true;
+    SharedPreferences _prefs = await SharedPreferences.getInstance();
+
+    bool? result = _prefs.getBool("active");
+
+    return result ?? true;
   }
 }
